63a36d9fe6568545e7f5ccf58d2493f17146cde5
Samantha <samanthabremner@bitbucket.org>
2020-11-18 21:16:03 +0200

    Fixing solidity contract

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,3 +1,4 @@
+SPDX-License-Identifier: GPL-3.0-or-later
 /*
     This exercise has been updated to use Solidity version 0.6.12
     Breaking changes from 0.5 to 0.6 can be found here: 
@@ -50,7 +51,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function () external payable {
+    fallback() external payable {
         revert();
     }
 
